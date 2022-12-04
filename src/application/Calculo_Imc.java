d0df84554573a32446ad666676b5a9f7e508b3a6
IgorScotti <igorscoti@hotmail.com>
2022-12-04 18:49:07 -0300

    Calculo do IMC finalizado

--- src/application/Calculo_Imc.java
+++ src/application/Calculo_Imc.java
@@ -16,7 +16,25 @@ public class Calculo_Imc {
 		System.out.print ("Digite a sua Altura: ");
 		double altura = sc.nextDouble();
 		
+		System.out.println();
 		
+		double imc = peso / (altura * altura);
+		
+		System.out.printf("Seu IMC : %.2f%n",imc);
+				
+		if (imc < 18.5) {
+			System.out.println("Baixo Peso ");
+		} 
+		else if (imc > 18.5 && imc < 24.99) {
+			System.out.println("Normal");
+		}
+		else if (imc >= 25 && imc < 29.99) {
+			System.out.println("Sobrepeso ");
+			
+		}
+		else {
+			System.out.println("Obesidade ");
+		}
 		
 		sc.close();
 
