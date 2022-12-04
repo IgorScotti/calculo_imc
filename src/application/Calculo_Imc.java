3af592524b0ecb057c997f09a2f7ae4c5bae28b4
IgorScotti <igorscoti@hotmail.com>
2022-12-04 10:55:32 -0300

    Adicionei a entrada do peso e altura

--- src/application/Calculo_Imc.java
+++ src/application/Calculo_Imc.java
@@ -1,9 +1,24 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Calculo_Imc {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner (System.in);
+		Locale.setDefault(Locale.US);
+		
+		
+		System.out.print ("Digite o seu peso: ");
+		double peso = sc.nextDouble();
+		
+		System.out.print ("Digite a sua Altura: ");
+		double altura = sc.nextDouble();
+		
+		
+		
+		sc.close();
 
 	}
 
